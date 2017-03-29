c24c9fc21f550b8cb23c22e9950d31886e2e90b0
xcwil <xcwillsnow@gmail.com>
2017-03-29 18:00:15 +0800

    test pass

--- Bi_rnn.py
+++ Bi_rnn.py
@@ -17,11 +17,63 @@ n_hidden = 256
 n_classes = 10
 
 #输入
-x = tf.palceholder("float",[None,n_steps,n_input])  #二维结构 None个（长，宽）
+x = tf.placeholder("float",[None,n_steps,n_input])  #二维结构 batch_size个（长，宽）
 y = tf.placeholder("float",[None,n_classes])
 
 #sofrmax weight bias
-weights = tf.Variable(tf.random_normal([2*n_hideen,n_classes]))
-bias = tf.Variable(tf.random_normal([n_classes]))
+weights = tf.Variable(tf.random_normal([2*n_hidden,n_classes]))
+biases = tf.Variable(tf.random_normal([n_classes]))
 
-def
+def BiRNN(x,weights,biases):
+
+    x = tf.transpose(x,[1,0,2]) # 变成n_step在前面，后面要按n_step分割
+    x = tf.reshape(x,[-1,n_input])  #变成二维的，n_step*batch_size
+    x = tf.split(x,n_steps)  #分割成n_step个
+
+    lstm_fw_cell = tf.contrib.rnn.BasicLSTMCell(n_hidden,forget_bias=1.0)
+    lstm_bw_cell = tf.contrib.rnn.BasicLSTMCell(n_hidden,forget_bias=1.0)
+
+    output, _, _ = tf.contrib.rnn.static_bidirectional_rnn(
+        lstm_fw_cell,
+        lstm_bw_cell,
+        x,
+        dtype=tf.float32)
+    return tf.matmul(output[-1],weights) + biases
+
+#生成一个BiRNN
+pred = BiRNN(x,weights,biases)
+#cost function
+cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=pred,labels=y))
+#优化器
+optimizer = tf.train.AdamOptimizer(learning_rate= learning_rate).minimize(cost)
+
+correct_pred = tf.equal(tf.argmax(pred,1),tf.argmax(y,1))  #返回最大值的index 然后做对比
+accuracy = tf.reduce_mean(tf.cast(correct_pred,tf.float32))
+
+init = tf.global_variables_initializer()
+
+
+with tf.Session() as sess:
+    sess.run(init)
+    step = 1
+    while step * batch_size < max_samples:
+        batch_x,batch_y = mnist.train.next_batch(batch_size)
+        batch_x = batch_x.reshape((batch_size,n_steps,n_input)) #用的python的reshape
+        sess.run(optimizer,feed_dict={x:batch_x,y:batch_y})
+        if step % display_step ==0:
+            acc = sess.run(accuracy,feed_dict={x:batch_x,y:batch_y})
+            loss = sess.run(cost,feed_dict={x:batch_x,y:batch_y})
+            #acc,loss = sess.run([accuracy,cost], feed_dict={x: batch_x, y: batch_y})
+            print("Iter "+str(step*batch_size) + ", Minibatach loss= " + \
+                  "{:.6f}".format(loss) + ", Training Accuracy= " + \
+                  "{:.5f}".format(acc))
+        step += 1
+
+    print("Optimazation Finished")
+
+
+    test_len = 10000
+    test_data = mnist.test.images[:test_len].reshape((-1,n_steps,n_input))
+    test_label = mnist.test.labels[:test_len]
+    print("testing Accuracy:" ,
+      sess.run(accuracy,feed_dict={x:batch_x,y:batch_y}))
